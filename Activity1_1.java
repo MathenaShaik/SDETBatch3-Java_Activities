65724d48bada1d3c8961e44c34b8d0aad3b0fa19
MathenaShaik <70267260+MathenaShaik@users.noreply.github.com>
2020-08-26 16:34:46 +0530

    Activity1_1

---
+++ Activity1_1.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Activity1_1 {
+
+	public static void main(String[] args) {
+		Car toyota = new Car();
+		toyota.make = 2014;
+		toyota.color = "Black";
+		toyota.transmission = "Manual";
+		
+		//Calling methods from Car Class
+		toyota.DisplayCharacteristics();
+		toyota.accelerate();
+		toyota.brake();
+				
+	}
+	
+}
